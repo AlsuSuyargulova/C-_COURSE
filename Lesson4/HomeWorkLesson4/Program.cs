3864e87a076f02f461c84fc2f785fba0a401d98a
Alsu Suyargulova <Cuyargulova@mail.ru>
2024-01-01 19:42:32 +0500

    Home_work_4

--- Lesson4/HomeWorkLesson4/Program.cs
+++ Lesson4/HomeWorkLesson4/Program.cs
@@ -1,73 +1,73 @@
-﻿// Задача 1: 
-// Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
-// Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+﻿// // Задача 1: 
+// // Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+// // Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
 
-Console.Clear();
-while (true)
-{
-    Console.Write("Введите число или 'q' для выхода: ");
-    string input = Console.ReadLine();
-    if (input == "q")
-    {
-        break;
-    }
-    int number;
-    if (int.TryParse(input, out number))
-    {
-        int sum = 0;
-        while (number > 0)
-        {
-            sum += number % 10;
-            number /= 10;
-        }
-        if (sum % 2 == 0)
-        {
-            Console.WriteLine("[STOP]");
-            break;
-        }
-    }
-    else
-    {
-        Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число или 'q'.");
-    }
-}
+// Console.Clear();
+// while (true)
+// {
+//     Console.Write("Введите число или 'q' для выхода: ");
+//     string input = Console.ReadLine();
+//     if (input == "q")
+//     {
+//         break;
+//     }
+//     int number;
+//     if (int.TryParse(input, out number))
+//     {
+//         int sum = 0;
+//         while (number > 0)
+//         {
+//             sum += number % 10;
+//             number /= 10;
+//         }
+//         if (sum % 2 == 0)
+//         {
+//             Console.WriteLine("[STOP]");
+//             break;
+//         }
+//     }
+//     else
+//     {
+//         Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число или 'q'.");
+//     }
+// }
 
-// Задача 2: 
-// Задайте массив заполненный случайными трёхзначными числами. 
-// Напишите программу, которая покажет количество чётных чисел в массиве.
+// // Задача 2: 
+// // Задайте массив заполненный случайными трёхзначными числами. 
+// // Напишите программу, которая покажет количество чётных чисел в массиве.
 
-Console.Clear();
-int[] numbers = new int[10] { 112, 386, 504, 915, 171, 902, 551, 181, 153, 150 };
-int count = 0;
-for (int i = 0; i < numbers.Length; i++)
-{
-    if (numbers[i] % 2 == 0)
-    {
-        count++;
-    }
-}
-Console.WriteLine($"Количество четных чисел в массиве: {count}");
+// Console.Clear();
+// int[] numbers = new int[10] { 112, 386, 504, 915, 171, 902, 551, 181, 153, 150 };
+// int count = 0;
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] % 2 == 0)
+//     {
+//         count++;
+//     }
+// }
+// Console.WriteLine($"Количество четных чисел в массиве: {count}");
 
-// Задача 3: 
-// Напишите программу, которая перевернёт одномерный массив 
-// (первый элемент станет последним, второй – предпоследним и т.д.)
+// // Задача 3: 
+// // Напишите программу, которая перевернёт одномерный массив 
+// // (первый элемент станет последним, второй – предпоследним и т.д.)
 
-Console.Clear();
-int[] numbers = { 112, 386, 504, 915, 171, 902, 551, 181, 153, 150 };
-int temp;
-Console.Write("Исходный массив: ");
-foreach (int number in numbers)
-{
-    Console.Write(number + " ");
-}
-for (int i = 0; i < numbers.Length / 2; i++)
-{
-    temp = numbers[i];
-    numbers[i] = numbers[numbers.Length - 1 - i];
-    numbers[numbers.Length - 1 - i] = temp;
-}
-Console.Write("\nПеревернутый массив: ");
-foreach (int number in numbers)
-{
-    Console.Write(number + " ");
-}
+// Console.Clear();
+// int[] numbers = { 112, 386, 504, 915, 171, 902, 551, 181, 153, 150 };
+// int temp;
+// Console.Write("Исходный массив: ");
+// foreach (int number in numbers)
+// {
+//     Console.Write(number + " ");
+// }
+// for (int i = 0; i < numbers.Length / 2; i++)
+// {
+//     temp = numbers[i];
+//     numbers[i] = numbers[numbers.Length - 1 - i];
+//     numbers[numbers.Length - 1 - i] = temp;
+// }
+// Console.Write("\nПеревернутый массив: ");
+// foreach (int number in numbers)
+// {
+//     Console.Write(number + " ");
+// }
